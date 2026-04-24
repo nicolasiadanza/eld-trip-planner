ba148efb56b196923f1b8ca3f1bd149aa420b67b
Your Name <you@example.com>
2026-04-24 20:12:21 -0300

    fix: open allowed hosts for production debug

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -1,6 +1,9 @@
 from .base import *
 import os
-DEBUG = False
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
+DEBUG = True  # Cambiar a True para habilitar el modo de depuración
+ALLOWED_HOSTS = ['*']  # Permitir todos los hosts
+APPEND_SLASH = True  # Asegurar que APPEND_SLASH esté configurado como True
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
